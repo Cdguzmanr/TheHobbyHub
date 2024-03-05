842821a76b61e57427065a3cde90c7dee5ee4424
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-03-04 21:15:35 -0600

    Fixed utUser

--- TheHobbyHub.BL.Test/utUser.cs
+++ TheHobbyHub.BL.Test/utUser.cs
@@ -4,8 +4,105 @@ using TheHobbyHub.BL.Models;
 namespace TheHobbyHub.BL.Test
 {
     [TestClass]
+    public class utUser
+    {
+
+        public void Seed()
+        {
+            UserManager.Seed();
+        }
+
+
+        [TestMethod]
+        public void InsertTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void LoginFailureNoUserName()
+        {
+            try
+            {
+                Seed();
+                Assert.IsTrue(UserManager.Login(new User { UserName = "", Password = "maple" }));
+            }
+            catch (LoginFailureException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+
+        [TestMethod]
+        public void LoginFailureBadPassword()
+        {
+            try
+            {
+                Seed();
+                Assert.IsTrue(UserManager.Login(new User { UserName = "bfoote", Password = "wrong" }));
+            }
+            catch (LoginFailureException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void LoginFailureBadUSerId()
+        {
+            try
+            {
+                Seed();
+                Assert.IsTrue(UserManager.Login(new User { UserName = "wrong", Password = "maple" }));
+            }
+            catch (LoginFailureException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void LoginFailureNoPassword()
+        {
+            try
+            {
+                Seed();
+                Assert.IsTrue(UserManager.Login(new User { UserName = "bfoote", Password = "" }));
+            }
+            catch (LoginFailureException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+    }
+    /**
+    [TestClass]
     public class utUser : utBase<User>
     {
+
+
+
+
+
+        
         [TestMethod]
         public void LoadTest()
         {
@@ -62,4 +159,5 @@ namespace TheHobbyHub.BL.Test
 
         //Add Tests for logins
     }
+    */
 }
