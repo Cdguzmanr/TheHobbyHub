c07a47e633564446d98b8b4ef958bc8c143a3664
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-04-22 15:34:46 -0500

    fixing massive merge mess up

--- TheHobbyHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TheHobbyHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
