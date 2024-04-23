4e55fe73a312dd8687f3d1ba8cb3a757061730fd
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-04-22 21:28:57 -0500

    pushing migrations

---
+++ TheHobbyHub.PL/Migrations/20240423022725_CreateDatabase.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TheHobbyHub.PL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblAddress",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PostalAddress = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    City = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    State = table.Column<string>(type: "varchar(5)", unicode: false, maxLength: 5, nullable: false),
+                    Zip = table.Column<string>(type: "varchar(12)", unicode: false, maxLength: 12, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblAddress_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblHobby",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HobbyName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Type = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblHobby_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUser",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    UserName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Image = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUser_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblCompany",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCompany_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "fk_tblCompany_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "tblAddress",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblCompany_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUserHobby",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HobbyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUserHobby_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "fk_tblUserHobby_HobbyId",
+                        column: x => x.HobbyId,
+                        principalTable: "tblHobby",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblUserHobby_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblEvent",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AddressId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HobbyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Image = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblEvent_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "fk_tblEvent_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "tblAddress",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblEvent_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "tblCompany",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblEvent_HobbyId",
+                        column: x => x.HobbyId,
+                        principalTable: "tblHobby",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblEvent_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblFriend",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblFriend_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "fk_tblFriend_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "tblCompany",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "fk_tblFriend_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblAddress",
+                columns: new[] { "Id", "City", "PostalAddress", "State", "Zip" },
+                values: new object[,]
+                {
+                    { new Guid("bfd1c1ae-e994-4864-8315-32d0946fce42"), "Othertown", "456 Elm St", "NY", "54321" },
+                    { new Guid("f75906ff-041a-4d4a-a233-8fa6fc571883"), "Somewhere", "789 Oak St", "TX", "67890" },
+                    { new Guid("fc09e2c5-791d-4c21-9488-468b5ba3ce35"), "Anytown", "123 Main St", "CA", "12345" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblHobby",
+                columns: new[] { "Id", "Description", "HobbyName", "Type" },
+                values: new object[,]
+                {
+                    { new Guid("3e81983c-f1d4-413c-9e67-73c6378f97be"), "stick", "Golf", "outdoor" },
+                    { new Guid("5301a072-48e2-430f-9414-93cab2ab6d56"), "Run", "Running", "Outdoor" },
+                    { new Guid("c68d8512-e3e1-44fc-8b33-434125c33bf2"), "Gyyymm", "Gym", "Indoor" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblUser",
+                columns: new[] { "Id", "Email", "FirstName", "Image", "LastName", "Password", "PhoneNumber", "UserName" },
+                values: new object[,]
+                {
+                    { new Guid("3c5d279f-cfa8-46e8-9812-0682a6d1055d"), "Alexr@gmail.com", "Alex", "image.jpg", "Rosas", "qUqP5cyxm6YcTAhz05Hph5gvu9M=", "2627459097", "Arosas" },
+                    { new Guid("412215f2-573e-4f1b-9807-9631471aa97d"), "sf@gmail.com", "sam", "sammy.jpg", "fisher", "qUqP5cyxm6YcTAhz05Hph5gvu9M=", "1111111111", "sammyfish" },
+                    { new Guid("437cc2b5-21e6-4426-a8dd-8fa4c492a788"), "bfoote@fvtc.edu", "Brian", "image.jpg", "Foote", "pYfdnNb8sO0FgS4H0MRSwLGOIME=", "3333333333", "bfoote" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblCompany",
+                columns: new[] { "Id", "AddressId", "CompanyName", "Description", "UserId" },
+                values: new object[,]
+                {
+                    { new Guid("b60ce81c-039d-4a36-8625-2ba39882f3e2"), new Guid("f75906ff-041a-4d4a-a233-8fa6fc571883"), "Company C", "about company C", new Guid("412215f2-573e-4f1b-9807-9631471aa97d") },
+                    { new Guid("cb66b796-8731-4465-b1f2-c3cb79867f0a"), new Guid("bfd1c1ae-e994-4864-8315-32d0946fce42"), "Company B", "about company B", new Guid("437cc2b5-21e6-4426-a8dd-8fa4c492a788") },
+                    { new Guid("e8e413e1-aaca-40b4-a14b-cc7a564f20ea"), new Guid("fc09e2c5-791d-4c21-9488-468b5ba3ce35"), "Company A", "about company A", new Guid("3c5d279f-cfa8-46e8-9812-0682a6d1055d") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblUserHobby",
+                columns: new[] { "Id", "HobbyId", "UserId" },
+                values: new object[,]
+                {
+                    { new Guid("25804229-67be-4cf2-aa9f-1b580fe0bcc0"), new Guid("5301a072-48e2-430f-9414-93cab2ab6d56"), new Guid("412215f2-573e-4f1b-9807-9631471aa97d") },
+                    { new Guid("691c9a1d-1e04-491d-9018-ba61d9966086"), new Guid("c68d8512-e3e1-44fc-8b33-434125c33bf2"), new Guid("3c5d279f-cfa8-46e8-9812-0682a6d1055d") },
+                    { new Guid("949ebcfa-0711-4456-8cc9-33655f9c2afd"), new Guid("3e81983c-f1d4-413c-9e67-73c6378f97be"), new Guid("437cc2b5-21e6-4426-a8dd-8fa4c492a788") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblEvent",
+                columns: new[] { "Id", "AddressId", "CompanyId", "Date", "Description", "HobbyId", "Image", "UserId" },
+                values: new object[,]
+                {
+                    { new Guid("892055b0-e034-490b-b0a5-38c1b4967f0b"), new Guid("bfd1c1ae-e994-4864-8315-32d0946fce42"), new Guid("cb66b796-8731-4465-b1f2-c3cb79867f0a"), new DateTime(2024, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Event B", new Guid("3e81983c-f1d4-413c-9e67-73c6378f97be"), "golf.jpg", new Guid("437cc2b5-21e6-4426-a8dd-8fa4c492a788") },
+                    { new Guid("aaebf203-1b41-4fde-9725-f6f8e5680354"), new Guid("f75906ff-041a-4d4a-a233-8fa6fc571883"), new Guid("b60ce81c-039d-4a36-8625-2ba39882f3e2"), new DateTime(2024, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Event C", new Guid("5301a072-48e2-430f-9414-93cab2ab6d56"), "running.jpeg", new Guid("412215f2-573e-4f1b-9807-9631471aa97d") },
+                    { new Guid("f82933c7-7bfe-45d1-b860-25d2e63dd936"), new Guid("fc09e2c5-791d-4c21-9488-468b5ba3ce35"), new Guid("e8e413e1-aaca-40b4-a14b-cc7a564f20ea"), new DateTime(2024, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Event A", new Guid("c68d8512-e3e1-44fc-8b33-434125c33bf2"), "gym.jpeg", new Guid("3c5d279f-cfa8-46e8-9812-0682a6d1055d") }
+                });
+
+            migrationBuilder.InsertData(
+                table: "tblFriend",
+                columns: new[] { "Id", "CompanyId", "UserId" },
+                values: new object[,]
+                {
+                    { new Guid("093191bb-0bf3-4e9b-9460-3b04f84b44c6"), new Guid("b60ce81c-039d-4a36-8625-2ba39882f3e2"), new Guid("412215f2-573e-4f1b-9807-9631471aa97d") },
+                    { new Guid("38ed1696-be52-4747-954c-5fd01a9b4833"), new Guid("e8e413e1-aaca-40b4-a14b-cc7a564f20ea"), new Guid("3c5d279f-cfa8-46e8-9812-0682a6d1055d") },
+                    { new Guid("3e441f2a-1656-4bf9-b6d1-76b2b6a6bf65"), new Guid("cb66b796-8731-4465-b1f2-c3cb79867f0a"), new Guid("437cc2b5-21e6-4426-a8dd-8fa4c492a788") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblCompany_AddressId",
+                table: "tblCompany",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblCompany_UserId",
+                table: "tblCompany",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblEvent_AddressId",
+                table: "tblEvent",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblEvent_CompanyId",
+                table: "tblEvent",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblEvent_HobbyId",
+                table: "tblEvent",
+                column: "HobbyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblEvent_UserId",
+                table: "tblEvent",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblFriend_CompanyId",
+                table: "tblFriend",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblFriend_UserId",
+                table: "tblFriend",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblUserHobby_HobbyId",
+                table: "tblUserHobby",
+                column: "HobbyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblUserHobby_UserId",
+                table: "tblUserHobby",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblEvent");
+
+            migrationBuilder.DropTable(
+                name: "tblFriend");
+
+            migrationBuilder.DropTable(
+                name: "tblUserHobby");
+
+            migrationBuilder.DropTable(
+                name: "tblCompany");
+
+            migrationBuilder.DropTable(
+                name: "tblHobby");
+
+            migrationBuilder.DropTable(
+                name: "tblAddress");
+
+            migrationBuilder.DropTable(
+                name: "tblUser");
+        }
+    }
+}
